99ca636cc6a5400c29a7f3417f9c92cd468f04b1
Marco Antonio <marco.agd.2013@gmail.com>
2023-11-20 12:15:49 -0300

    first commit

---
+++ vendor/julfiker/laravel-sqlsrv/src/SqlSrvServiceProvider.php
@@ -0,0 +1,60 @@
+<?php
+namespace Julfiker\SqlSrv;
+
+use Julfiker\SqlSrv\Connection\SqlServerConnection;
+use Illuminate\Database\Connection;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * A service provider to integrate with laravel application
+ *
+ * @author Julfiker <mail.julfiker@gmail.com>
+ * Class SqlSrvServiceProvider
+ * @package Julfiker\SqlSrv
+ */
+class SqlSrvServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Boot Oci8 Provider.
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+        Connection::resolverFor('sqlsrv', function ($connection, $database, $prefix, $config) {
+            $db = new SqlServerConnection($connection, $database, $prefix, $config);
+
+            if (! empty($config['skip_session_vars'])) {
+                return $db;
+            }
+
+            return $db;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return string[]
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
